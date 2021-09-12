9d3cb3845cd67bdd5b2491c7bb0c452a514338fa
kyomin <kim031504@naver.com>
2021-09-13 02:52:06 +0900

    숫자야구 React.memo로 렌더링 최적화

--- 숫자야구/Try.jsx
+++ 숫자야구/Try.jsx
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
+import React, { memo } from 'react';
 
-const Try = ({ tryInfo }) => {
+// memo를 사용하면, state나 props가 이전 상태와 달라졌을 때에만 다시 렌더링 한다.
+const Try = memo(({ tryInfo }) => {
   return (
     <li>
       <div>{tryInfo.try}</div>
       <div>{tryInfo.result}</div>
     </li>
   );
-}
+})
 
 export default Try;
