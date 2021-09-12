527a1aab4a3087679e8c4bd2a5d441176a426eec
kyomin <kim031504@naver.com>
2021-09-13 02:28:11 +0900

    숫자야구 hooks로 만들기

--- 숫자야구/NumberBaseball.jsx
+++ 숫자야구/NumberBaseball.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Try from './Try';
 
 function getNumbers() {   // 숫자 네 개를 겹치지 않고 랜덤하게 뽑는 함수
@@ -13,93 +13,81 @@ function getNumbers() {   // 숫자 네 개를 겹치지 않고 랜덤하게 뽑
   return array;
 }
 
-class NumberBaseball extends Component {
-  state = {
-    result: '',
-    value: '',
-    answer: getNumbers(),
-    tries: []
-  }
+const NumberBaseball = () => {
+  const [result, setResult] = useState('');
+  const [value, setValue] = useState('');
+  const [answer, setAnswer] = useState(getNumbers());
+  const [tries, setTries] = useState([]);
 
-  onSubmitFrom = (e) => {
+  const onSubmitFrom = (e) => {
     e.preventDefault();
 
-    if (this.state.value === this.state.answer.join('')) {  // 바로 맞췄으면!
-      this.setState({
-        result: '홈런!',
-        tries: [...this.state.tries, { try: this.state.value, result: '홈런!' }]
+    if (value === answer.join('')) {  // 바로 맞췄으면!
+      setResult('홈런!');
+      setTries((prevTries) => {   // 이전 값을 이용해 현재 값을 만드는 경우
+        return [...prevTries, { try: value, result: '홈런!' }]
       });
 
       alert('게임을 다시 시작합니다!');
 
-      this.setState({
-        value: '',
-        answer: getNumbers(),
-        tries: []
-      });
+      setValue('');
+      setAnswer(getNumbers());
+      setTries([]);
     } else {    // 답이 틀렸으면
-      const answerArray = this.state.value.split('').map((v) => parseInt(v));
+      const answerArray = value.split('').map((v) => parseInt(v));
       let strike = 0;
       let ball = 0;
 
-      if (this.state.tries.length >= 9) {   // 10번 틀렸다면
-        this.setState({
-          result: `10번 넘게 틀려서 실패! 답은 ${this.state.answer.join(',')}였습니다!`
-        });
+      if (tries.length >= 9) {   // 10번 틀렸다면
+        setResult(`10번 넘게 틀려서 실패! 답은 ${answer.join(',')}였습니다!`);
 
         alert('게임을 다시 시작합니다!');
 
-        this.setState({
-          value: '',
-          answer: getNumbers(),
-          tries: []
-        });
+        setValue('');
+        setAnswer(getNumbers());
+        setTries([]);
       } else {
         for (let i=0; i<4; i++) {
-          if (answerArray[i] === this.state.answer[i]) {
+          if (answerArray[i] === answer[i]) {
             strike += 1;
-          } else if (this.state.answer.includes(answerArray[i])) {
+          } else if (answer.includes(answerArray[i])) {
             ball += 1;
           }
         }
 
-        this.setState({
-          tries: [...this.state.tries, { try: this.state.value, result: `${strike} 스트라이크, ${ball} 볼입니다.` }],
-          value: ''
+        setTries((prevTries) => {
+          return [...prevTries, { try: value, result: `${strike} 스트라이크, ${ball} 볼입니다.` }]
         });
+        setValue('');
       }
     }
   }
 
-  onChangeInput = (e) => {
-    this.setState({
-      value: e.target.value
-    });
+  const onChangeInput = (e) => {
+    setValue(e.target.value);
   }
 
-  render() {
-    return (
-      <>
-        <h1>숫자야구 게임</h1>
-        <h2>{this.state.result}</h2>
-        <form onSubmit={this.onSubmitFrom}>
-          <input maxLength={4} value={this.state.value} onChange={this.onChangeInput} />
-        </form>
-        <div>시도: {this.state.tries.length}</div>
-        <ul>
-          {this.state.tries.map((v, i) => {
-            return (
-              <Try
-                key={i}
-                tryInfo={v}
-                index={i}
-              />
-            );
-          })}
-        </ul>
-      </>
-    );
-  }
+  return (
+    <>
+      <h1>숫자야구 게임</h1>
+      <h2>{result}</h2>
+      <form onSubmit={onSubmitFrom}>
+        <input maxLength={4} value={value} onChange={onChangeInput} />
+      </form>
+      <div>시도: {tries.length}</div>
+      <ul>
+        {tries.map((v, i) => {
+          return (
+            <Try
+              key={i}
+              tryInfo={v}
+              index={i}
+            />
+          );
+        })}
+      </ul>
+    </>
+  ); 
 }
 
 export default NumberBaseball;
