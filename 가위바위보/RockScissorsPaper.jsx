a2927baf0b5d31edd3aed742750f7ad056deb6a9
kyomin <kim031504@naver.com>
2021-09-17 21:00:20 +0900

    가위바위보 게임 고차 함수로 클릭 이벤트 바인딩

--- 가위바위보/RockScissorsPaper.jsx
+++ 가위바위보/RockScissorsPaper.jsx
@@ -66,7 +66,7 @@ class RockScissorsPaper extends Component {
     }
   }
 
-  onClickBtn = (choice) => {
+  onClickBtn = (choice) => () => {
     const { imgCord, flag } = this.state;
 
     if (flag) {
@@ -119,9 +119,9 @@ class RockScissorsPaper extends Component {
       <>
         <div id="computer" style={{ background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCord} 0` }}></div>
         <div>
-          <button id="rock" className="btn" onClick={() => this.onClickBtn('바위')}>바위</button>
-          <button id="scissor" className="btn" onClick={() => this.onClickBtn('가위')}>가위</button>
-          <button id="paper" className="btn" onClick={() => this.onClickBtn('보')}>보</button>
+          <button id="rock" className="btn" onClick={this.onClickBtn('바위')}>바위</button>
+          <button id="scissor" className="btn" onClick={this.onClickBtn('가위')}>가위</button>
+          <button id="paper" className="btn" onClick={this.onClickBtn('보')}>보</button>
         </div>
         <div>{result}</div>
         <div>현재 {score}점</div>
