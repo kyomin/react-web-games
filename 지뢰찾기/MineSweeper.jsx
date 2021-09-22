8b4b3ffc6f7d030ca80dcaa3eb93d4848c0b9135
kyomin <kim031504@naver.com>
2021-09-22 23:45:39 +0900

    지뢰찾기 만들기 준비

---
+++ 지뢰찾기/MineSweeper.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MineSweeper = () => {
+
+};
+
+export default MineSweeper;
---
+++ 지뢰찾기/client.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import MineSweeper from './MineSweeper';
+
+ReactDom.render(<MineSweeper />, document.querySelector('#root'));
